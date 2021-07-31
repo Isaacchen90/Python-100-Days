c1f8c86e9c159c4001ce32e45ff7dfc5ee14cd65
Isaacchen90 <120003953@qq.com>
2021-07-31 15:13:38 +0800

    Day02 还未学完

---
+++ IsaacChen_Python/Day02.py
@@ -0,0 +1,31 @@
+#整型：int
+#浮点型：float
+#字符串：string '' ""
+#布尔型： bool True False  例如3 < 5 会产生布尔值为True
+#负数型：非常用，3+5j
+a = 321
+b = 12
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+c = 100
+d = 12.345
+e = 1 + 5j
+f = "hello,world"
+g = True
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+#类型转换
+#int()：将一个数值或字符串转换成整数，可以指定进制。
+#float()：将一个字符串转换成浮点数。
+#str()：将指定的对象转换成字符串形式，可以指定编码。
+#chr()：将整数转换成该编码对应的字符串（一个字符）。
+#ord()：将字符串（一个字符）转换成对应的编码（整数）。
+
+
+
+
+# 你好  我好大家好  你是我的荣耀  杨洋带你打王者
